be3b06cda0a515cba168a5db101e1a83d36eab3e
Taiwo Abiodun <taiwobiodun@gmail.com>
2024-02-07 16:49:08 +0100

    add logout functions and page redirections after registrations and login with home page details

--- marketplace/__init__.py
+++ marketplace/__init__.py
@@ -13,6 +13,8 @@ app.app_context().push()
 bcrypt = Bcrypt(app)
 
 login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 
 
 from marketplace import routes
--- marketplace/routes.py
+++ marketplace/routes.py
@@ -2,14 +2,15 @@ from marketplace import app, db
 from flask import render_template, redirect, url_for, flash
 from marketplace.models import Item, User
 from marketplace.forms import RegistrationForm, LoginForm
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 
 @app.route('/')
 @app.route('/home')
 def home_page():
-    return render_template('home.html')
+    return render_template('home.html', title='Welcome to Release & Acquire')
 
 @app.route('/market')
+@login_required
 def market_page():
     items = Item.query.all()
     return render_template('market.html', title='Market', items=items)
@@ -21,6 +22,8 @@ def register_page():
         user_to_create = User(username=form.username.data, email=form.email.data, password=form.password.data)
         db.session.add(user_to_create)
         db.session.commit()
+        login_user(user_to_create)
+        flash(f'Account created successfully! You are logged in as: {user_to_create.username}', category='success')
         return redirect(url_for('market_page'))
     if form.errors != {}: #Validation error handling
         for err_msg in form.errors.values():
@@ -39,3 +42,10 @@ def login_page():
         else:
             flash(f'Email and Password are not match! Please retry', category='warning')
     return render_template('login.html', title='Login', form=form)
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash(f'You have been logged out!', category='info')
+    return redirect(url_for('home_page'))
+
