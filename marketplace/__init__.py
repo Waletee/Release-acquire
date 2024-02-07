c093a532cfa803eb91cc043cc887b5edf3f73c49
Taiwo Abiodun <taiwobiodun@gmail.com>
2024-02-07 12:14:13 +0100

    create and update registration and login page

--- marketplace/__init__.py
+++ marketplace/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ea05b6e38431cd809ac9652400549700'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///marketplace.db'
 db = SQLAlchemy(app)
 
--- marketplace/routes.py
+++ marketplace/routes.py
@@ -1,6 +1,7 @@
-from marketplace import app
-from flask import render_template
-from marketplace.models import Item
+from marketplace import app, db
+from flask import render_template, redirect, url_for
+from marketplace.models import Item, User
+from marketplace.forms import RegistrationForm, LoginForm
 
 @app.route('/')
 @app.route('/home')
@@ -11,3 +12,21 @@ def home_page():
 def market_page():
     items = Item.query.all()
     return render_template('market.html', title='Market', items=items)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_page():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data, email=form.email.data, password_hashed=form.password.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors != {}: #Validation error handling
+        for err_msg in form.errors.values():
+            print(f'There is an error creating a user: {err_msg}')
+    return render_template('register.html', title='Registration', form=form)
+
+@app.route('/login')
+def login_page():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
